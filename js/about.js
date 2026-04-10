760fef1e66129a3767c9fbb7e627ed68bc58ffd7
mohmed <mohmedkhalad390@gmail.com>
2026-04-10 21:04:37 +0200

    Upload project files

---
+++ js/about.js
@@ -0,0 +1,56 @@
+var navbar = document.getElementById("navbar");
+var loading = document.getElementById("loading");
+var navBtns = document.querySelectorAll(".nav-btns");
+window.addEventListener("scroll" , function(){
+var scrolled = window.scrollY
+ if(scrolled> 0.1 ) {
+    navbar.classList.add('top-fixed')
+ }
+  else if(scrolled <= 0 ){
+   navbar.classList.remove('top-fixed')
+  }
+}
+)
+
+$('.owl-carousel').owlCarousel({
+    items:4,
+    loop:true,
+    merge:true,
+    margin:10,
+    dots:true ,
+    dotsEach:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+          900:{
+            items:3,
+            nav:true
+        },
+        1100:{
+            items:4,
+            nav:true,
+          
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded", function() {
+      loading.style.display='none'
+})
+
+for(var i = 0 ; i<navBtns.length ; i++){
+    navBtns[i].addEventListener('click' , function(){
+        for(var j=0 ; j<navBtns.length ; j++){
+          navBtns[j].classList.remove('focus')        
+        }
+    this.classList.add("focus");
+    })
+}
